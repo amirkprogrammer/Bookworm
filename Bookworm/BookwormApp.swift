1422cdf3950e92a03a85b6685b9041d296fa99c7
amirkprogrammer <amir.m.kabiri@gmail.com>
2024-03-14 12:04:54 -0700

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Amir Kabiri on 3/14/24.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
