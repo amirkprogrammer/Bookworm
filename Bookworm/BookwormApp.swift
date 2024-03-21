952121fec9ecbe44fec90f508f8abcf2cb1ca620
amirkprogrammer <amir.m.kabiri@gmail.com>
2024-03-20 19:01:19 -0700

    - add detail view - add swipe to delete functionality - add function to delete from model context and return to navigation stack programmatically

--- Bookworm/BookwormApp.swift
+++ Bookworm/BookwormApp.swift
@@ -5,6 +5,7 @@
 //  Created by Amir Kabiri on 3/14/24.
 //
 
+import SwiftData
 import SwiftUI
 
 @main
@@ -13,5 +14,6 @@ struct BookwormApp: App {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: Book.self)
     }
 }
